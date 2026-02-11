18444ad63c3161a46cc046ed4ac6a406039db5e9
chloecrozier <chloecrozier@gmail.com>
2026-02-11 14:17:57 -0800

    multinode nccl mvp

--- nccl/containers/entrypoint.sh
+++ nccl/containers/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
-# Start sshd so MPI can SSH between workers (RunAI may inject authorized_keys at runtime)
+
+# Start sshd so MPI can SSH between workers
 if command -v sshd >/dev/null 2>&1; then
     /usr/sbin/sshd
 fi
+
 exec "$@"
